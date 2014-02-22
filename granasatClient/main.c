f829e7b441c9ee765df47e824076c71153d95bcc
M42 <mikrokosmos42@gmail.com>
2014-02-22 17:26:27 +0100

    Testing CPU temperature label. Using random integers to simulate real temperature.

--- granasatClient/main.c
+++ granasatClient/main.c
@@ -6,12 +6,22 @@
  *      Author: Mario Román
  */
 
+// GTK GUI library
 #include <gtk/gtk.h>
 #include <glib.h>
+// Random numbers
+#include <time.h>
+#include <stdlib.h>
+
+
+void refreshCPUTemperature(GtkLabel* temp_label);
+
 
 int main (int argc, char* argv[])
 {
 	GtkWidget* main_window;
+	GtkWidget* CPU_temperature_label;
+	const int REFRESH_INTERVAL = 100;
 
 	// Initialize GTK
 	gtk_init (&argc, &argv);
@@ -21,8 +31,29 @@ int main (int argc, char* argv[])
 	gtk_window_set_title (GTK_WINDOW (main_window), "Client");
 	gtk_widget_show(main_window);
 
+
+	// CPU Temperature label
+	CPU_temperature_label = gtk_label_new (NULL);
+	//gtk_timeout_add (REFRESH_INTERVAL, refreshCPUTemperature, CPU_temperature_label);
+
+	// Terminate application when window is destroyed
+	g_signal_connect (main_window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
 	// GTK event loop
 	gtk_main();
 
 	return 0;
+}
+
+
+void refreshCPUTemperature (GtkLabel* temperature_label) {
+	// Reads Raspberry CPU Temperature
+	// TO-DO
+	srand(time(NULL));
+	int new_temperature = rand();
+
+	// Refreshes temperature label
+	char buffer[100];
+	sprintf(buffer,"%d",new_temperature+1);
+	gtk_label_set_text(temperature_label, buffer);
 }
