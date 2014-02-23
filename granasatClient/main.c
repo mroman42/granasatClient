af11d04b9d572fa4c0a2a81bec5397f72ec63756
M42 <mikrokosmos42@gmail.com>
2014-02-23 13:22:23 +0100

    Displaying random CPU temperatures. A gtk fixed container has been added.

--- granasatClient/main.c
+++ granasatClient/main.c
@@ -14,27 +14,41 @@
 #include <stdlib.h>
 
 
-void refreshCPUTemperature(GtkLabel* temp_label);
+gboolean refreshCPUTemperature(GtkLabel* temp_label);
 
 
 int main (int argc, char* argv[])
 {
 	GtkWidget* main_window;
-	GtkWidget* CPU_temperature_label;
-	const int REFRESH_INTERVAL = 100;
+	GtkWidget* main_container;
+	GtkWidget* cpu_temperature_label;
+	const int REFRESH_INTERVAL = 10;
 
 	// Initialize GTK
 	gtk_init (&argc, &argv);
 
+
+
 	// Display the main window
 	main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (main_window), "Client");
+	gtk_container_set_border_width (GTK_CONTAINER (main_window), 10);
 	gtk_widget_show(main_window);
+
+	// Create a fixed container
+	main_container = gtk_fixed_new();
+	gtk_container_add(GTK_CONTAINER(main_window), main_container);
+	gtk_widget_show(main_container);
+
+
 
 
 	// CPU Temperature label
-	CPU_temperature_label = gtk_label_new (NULL);
-	//gtk_timeout_add (REFRESH_INTERVAL, refreshCPUTemperature, CPU_temperature_label);
+	cpu_temperature_label = gtk_label_new (NULL);
+	g_timeout_add (REFRESH_INTERVAL, (GSourceFunc) refreshCPUTemperature, (gpointer) cpu_temperature_label);
+	gtk_fixed_put (GTK_FIXED (main_container), cpu_temperature_label, 10, 10);
+	gtk_widget_show (cpu_temperature_label);
+
 
 	// Terminate application when window is destroyed
 	g_signal_connect (main_window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
@@ -46,7 +60,7 @@ int main (int argc, char* argv[])
 }
 
 
-void refreshCPUTemperature (GtkLabel* temperature_label) {
+gboolean refreshCPUTemperature (GtkLabel* temperature_label) {
 	// Reads Raspberry CPU Temperature
 	// TO-DO
 	srand(time(NULL));
@@ -54,6 +68,8 @@ void refreshCPUTemperature (GtkLabel* temperature_label) {
 
 	// Refreshes temperature label
 	char buffer[100];
-	sprintf(buffer,"%d",new_temperature+1);
+	sprintf(buffer,"CPU Temperature: %d ºC",(new_temperature+1)/10000000);
 	gtk_label_set_text(temperature_label, buffer);
+
+	return 1;
 }
