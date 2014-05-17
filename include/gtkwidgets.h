7c2d1c2de26acdf466501e7500a10512bde898f4
M42 <mromang08@gmail.com>
2014-05-17 17:19:33 +0200

    Gtk Widgets.

---
+++ include/gtkwidgets.h
@@ -0,0 +1,17 @@
+/**
+ * gtkwidgets.h
+ * granaSAT.Client
+ *
+ *  Created on: May 17, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef GTKWIDGETS_H
+#define GTKWIDGETS_H
+
+#include <gtk/gtk.h>
+
+GtkWidget* add_main_window ();
+
+#endif
---
+++ src/gtkwidgets.c
@@ -0,0 +1,13 @@
+/**
+ * gtkwidgets.c
+ * granaSAT.Client
+ *
+ *  Created on: May 17, 2014
+ *      Author: Mario Román
+ */
+
+#include <gtkwidgets.h>
+
+GtkWidget* add_main_window () {
+    
+}
