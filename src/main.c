c498679ec808259fc7d49e42f7bec35ea8c0f25a
M42 <mromang08@gmail.com>
2014-05-17 16:55:47 +0200

    gtkgui.h included

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 /**
  * main.c
- * granaSAT.Client 
+ * granaSAT.Client
  *
  *  Created on: May 16, 2014
  *      Author: Mario Román
  */
 
+#include <gtkgui.h>
+
+
 int main (int argc, char* argv[]) {
+
+    return 0;
 }
-
-
-
-
