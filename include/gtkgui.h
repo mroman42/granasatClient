7999b5ce60a82d082f426f66ecf92f1f9ab69151
M42 <mromang08@gmail.com>
2014-05-17 16:55:17 +0200

    gtkgui.h

---
+++ include/gtkgui.h
@@ -0,0 +1,15 @@
+/**
+ * main.c
+ * granaSAT.Client
+ *
+ *  Created on: May 17, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef GTKGUI_H
+#define GTKGUI_H
+
+#include <gtk/gtk.h>
+
+#endif
