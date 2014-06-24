e615bd3a245b9a4f6086b11bcb8247d664a49585
M42 <mromang08@gmail.com>
2014-06-24 20:42:05 +0200

    data.h & gtkrefresh.h

---
+++ include/data.h
@@ -0,0 +1,13 @@
+/**
+ * data.h
+ * granaSAT.Client
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef DATA_H
+#define DATA_H
+
+#endif
---
+++ include/gtkrefresh.h
@@ -0,0 +1,21 @@
+/**
+ * gtkrefresh.h
+ * granaSAT.Client
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef GTKREFRESH_H
+#define GTKREFRESH_H
+
+#include <gtk/gtk.h>
+#include "gtkwidgets.h"
+#include "data.h"
+
+static void add_timeouts() {
+    
+}
+
+#endif
