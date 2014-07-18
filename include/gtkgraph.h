ea1dd5ecbc3a212ff70d041e195441fe16d9b63a
M42 <mromang08@gmail.com>
2014-07-18 14:50:44 +0200

    New files gtkgraph.h draws the two graphs. gtksignals controls the redraw signals.

---
+++ include/gtkgraph.h
@@ -0,0 +1,58 @@
+/**
+ * gtkgraph.h
+ * granaSAT.Client
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef GTKGRAPH_H
+#define GTKGRAPH_H
+
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <cairo.h>
+#include "gtkgui.h"
+
+
+static void drawGraph (GtkWidget* widget, cairo_t* cr, gpointer user_data);
+static void add_plots (GtkBuilder* builder, int* magnetometer_measures, int* accelerometer_measures);
+
+
+static void add_plots (GtkBuilder* builder, int* magnetometer_measures, int* accelerometer_measures) {
+	
+}
+
+static void drawGraph (GtkWidget* widget, cairo_t* cr, gpointer user_data) {
+    // Measures
+	float* measures = (float*) user_data;
+
+	// Cairo initializing
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_paint(cr);
+
+	// Axis
+	const float HEIGHT = 200;
+	const float WIDTH  = 300;
+	const float SIN_60 = 0.8660254037844386;
+
+	cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+	cairo_move_to (cr, WIDTH/2,  2*HEIGHT/3);
+	cairo_line_to (cr, WIDTH/2,  0.0);
+	cairo_move_to (cr, WIDTH/2,  2*HEIGHT/3);
+	cairo_line_to (cr, WIDTH/2 + 2*HEIGHT/3*SIN_60,HEIGHT);
+	cairo_move_to (cr, WIDTH/2,  2*HEIGHT/3);
+	cairo_line_to (cr, WIDTH/2 - 2*HEIGHT/3*SIN_60,HEIGHT);
+	cairo_stroke (cr);
+
+	// Vector
+	cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+	cairo_move_to (cr, WIDTH/2, 2*HEIGHT/3);
+	cairo_line_to (cr, WIDTH/2 + SIN_60*measures[0]*4 -  SIN_60*measures[1]*4 , 2*HEIGHT/3-measures[2]*4);
+	cairo_stroke (cr);
+}
+
+
+
+#endif
---
+++ include/gtksignals.h
@@ -0,0 +1,25 @@
+/**
+ * gtksignals.h
+ * granaSAT.Client
+ *
+ *  Created on: Jul 18, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef GTKSIGNALS_H
+#define GTKSIGNALS_H
+
+#include <gtk/gtk.h>
+#include <glib.h>
+#include "gtkgui.h"
+#include "gtkgraph.h"
+#include "data.h"
+
+static void connect_all_signals (){
+    // Draw signals
+    g_signal_connect (G_OBJECT (drawing_area1), "draw", G_CALLBACK (drawGraph), MAG);
+    g_signal_connect (G_OBJECT (drawing_area2), "draw", G_CALLBACK (drawGraph), ACC);
+}
+
+#endif
