2352383fce431c8d0a011fa63aeefe84f9b34337
M42 <mromang08@gmail.com>
2014-07-25 12:58:47 +0200

    New protocol Using 0 as No-Command

--- include/protocol.h
+++ include/protocol.h
@@ -1,6 +1,6 @@
 /**
  * protocol.h
- * granaSAT.Client
+ * granaSAT.Client/granaSAT.Server
  *
  *  Created on: Jul 24, 2014
  *      Author: Alejandro García, Mario Román
@@ -11,13 +11,15 @@
 #define PROTOCOL_H
 
 
-#define MSG_PING          0
-#define MSG_END           1
-#define MSG_RESTART       2
+// COMMANDS
+#define MSG_PASS          0    // 0. No operation
+#define MSG_END           1    // 1. Ends connection
+#define MSG_RESTART       2    // 2. Restarts connection
+#define MSG_PING          3    // 3. Tests connection
 
-#define MSG_SET_STARS     20    // (+ int)
-#define MSG_SET_CATALOG   21    // (+ int)
-#define MSG_SET_PX_THRESH 22    // (+ int)
+#define MSG_SET_STARS     20   // 20. Sets centroids (+ int)
+#define MSG_SET_CATALOG   21   // 21. Sets catalog   (+ int)
+#define MSG_SET_PX_THRESH 22   // 22. Sets threshold (+ int)
 
 
 
