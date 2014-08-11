b62751a6f260c049e18d38d0f008b05a6dc8f276
M42 <mromang08@gmail.com>
2014-08-11 18:30:03 +0200

    Colours.h

---
+++ include/colours.h
@@ -0,0 +1,23 @@
+/**
+ * colours.h
+ * granaSAT.Client
+ *
+ *  Created on: Aug 11, 2014
+ *      Author: Mario Román
+ */
+
+// Avoids redefinition
+#ifndef COLOURS_H_
+#define COLOURS_H_
+
+#define KRED	"\033[31m"    /**@brief Red colour string for printing in the terminal*/
+#define KLRE	"\033[1;31m"  /**@brief Light red colour string for printing in the terminal*/
+#define KGRN	"\033[32m"    /**@brief Green colour string for printing in the terminal*/
+#define KYEL	"\033[33m"    /**@brief Yellow colour string for printing in the terminal*/
+#define KBLU	"\033[34m"    /**@brief Blue colour string for printing in the terminal*/
+#define KMAG	"\033[35m"    /**@brief Magenta colour string for printing in the terminal*/
+#define KCYN	"\033[36m"    /**@brief Cyan colour string for printing in the terminal*/
+#define KWHT	"\033[37m"    /**@brief White colour string for printing in the terminal*/
+#define KRES	"\033[0m"     /**@brief Reset colour string for printing in the terminal*/
+
+#endif
