055088094348b28a1ba161b764137fb9aa9aab47
M42 <mromang08@gmail.com>
2014-09-16 12:21:25 +0200

    [Log] Script retrieves measures

--- client.sh
+++ client.sh
@@ -4,7 +4,7 @@
 
 # Transforms the raw log into a clean log.
 # Erases "color" characters.
-raw_log() {
+clean_log() {
     sed 's/\\[32m//g 
          s/\\[0m//g
          s/\\[33//g
@@ -13,13 +13,13 @@ raw_log() {
          s/\\[36m//g
          s/\\[37m//g
          s/\\[31m//g
-         s/\\[1\;31m//g' < rawlog.log > granasatLog.log
+         s/\\[1\;31m//g' < ./log/rawlog.log > ./log/granasatLog.log
 }
 
 # Trap code that will be executed if the main program is interrupted.
 int_trap() {
     echo "[SCRIPT]: GranaSAT Client interrupted."
-    raw_log
+    clean_log
 }
 
 ## MAIN SCRIPT
@@ -28,10 +28,13 @@ trap int_trap INT
 
 # Runs the program and image viewer
 xdg-open ./images/image.bmp &
-(./bin/granasatClient | tee rawlog.log) || true
+(./bin/granasatClient | tee ./lograwlog.log) || true
 
 
 # Cleans the log
-raw_log
+clean_log
 
-# Magnetometer and accelerometer measures
+# Magnetometer, accelerometer and temperature measures
+grep "Magnetometer measures:" ./log/granasatLog.log | cut -d':' -f1,3,5  > ./log/magnetometer_measures.log
+grep "Accelerometer measures:" ./log/granasatLog.log | cut -d':' -f1,3,5 > ./log/accelerometer_measures.log
+grep "temperature" ./log/granasatLog.log | cut -d':' -f1,3,4,5 > ./log/temperatures.log
