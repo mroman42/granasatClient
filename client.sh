762155db65e72c6eb26885c1b0da3c9dbcc7392d
M42 <mromang08@gmail.com>
2014-09-25 17:03:27 +0200

    Time in log folder name

--- client.sh
+++ client.sh
@@ -2,6 +2,10 @@
 # This script can be used to execute granasatClient and obtain
 # a clean log of the complete execution.
 
+# Environment variables
+CLIENT=./bin/granasatClient
+LOG_FOLDR=./log
+
 # Transforms the raw log into a clean log.
 # Erases "color" characters.
 clean_log() {
@@ -13,7 +17,7 @@ clean_log() {
          s/\\[36m//g
          s/\\[37m//g
          s/\\[31m//g
-         s/\\[1\;31m//g' < ./log/rawlog.log > ./log/granasatLog.log
+         s/\\[1\;31m//g' < $LOG_FOLDR/rawlog.log > $LOG_FOLDR/granasatLog.log
 }
 
 # Trap code that will be executed if the main program is interrupted.
@@ -26,15 +30,20 @@ int_trap() {
 # Sets the trap.
 trap int_trap INT
 
+# Writes the time in the log folder
+LOG_FOLDR=$LOG_FOLDR`date +%s`
+mkdir $LOG_FOLDR
+echo "Log folder: $LOG_FOLDR"
+
 # Runs the program and image viewer
 xdg-open ./images/image.bmp &
-(./bin/granasatClient | tee ./lograwlog.log) || true
+($CLIENT | tee $LOG_FOLDR/rawlog.log) || true
 
 
 # Cleans the log
 clean_log
 
 # Magnetometer, accelerometer and temperature measures
-grep "Magnetometer measures:" ./log/granasatLog.log | cut -d':' -f1,3,5  > ./log/magnetometer_measures.log
-grep "Accelerometer measures:" ./log/granasatLog.log | cut -d':' -f1,3,5 > ./log/accelerometer_measures.log
-grep "temperature" ./log/granasatLog.log | cut -d':' -f1,3,4,5 > ./log/temperatures.log
+grep "Magnetometer measures:" $LOG_FOLDR/granasatLog.log | cut -d':' -f1,3,5  > $LOG_FOLDR/magnetometer_measures.log
+grep "Accelerometer measures:" $LOG_FOLDR/granasatLog.log | cut -d':' -f1,3,5 > $LOG_FOLDR/accelerometer_measures.log
+grep "temperature" $LOG_FOLDR/granasatLog.log | cut -d':' -f1,3,4,5 > $LOG_FOLDR/temperatures.log
